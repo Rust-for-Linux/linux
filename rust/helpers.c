fe61013fa4e8838522c92c380f61a64520bd3c76
Martin Rodriguez Reboredo <yakoyoku@gmail.com>
2023-02-22 16:00:08 -0300

    rust: xarray: Add an abstraction for XArray
    The XArray is an abstract data type which behaves like a very large
    array of pointers. Add a Rust abstraction for this data type.
    
    The initial implementation uses explicit locking on get operations and
    returns a guard which blocks mutation, ensuring that the referenced
    object remains alive. To avoid excessive serialization, users are
    expected to use an inner type that can be efficiently cloned (such as
    Arc<T>), and eagerly clone and drop the guard to unblock other users
    after a lookup.
    
    Future variants may support using RCU instead to avoid mutex locking.
    
    This abstraction also introduces a reservation mechanism, which can be
    used by alloc-capable XArrays to reserve a free slot without immediately
    filling it, and then do so at a later time. If the reservation is
    dropped without being filled, the slot is freed again for other users,
    which eliminates the need for explicit cleanup code.
    
    Signed-off-by: Martin Rodriguez Reboredo <yakoyoku@gmail.com>
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/helpers.c
+++ rust/helpers.c
@@ -39,6 +39,7 @@
 #include <linux/skbuff.h>
 #include <linux/uaccess.h>
 #include <linux/uio.h>
+#include <linux/xarray.h>
 
 __noreturn void rust_helper_BUG(void)
 {
@@ -349,6 +350,42 @@ void rust_helper_mutex_lock(struct mutex *lock)
 	mutex_lock(lock);
 }
 EXPORT_SYMBOL_GPL(rust_helper_mutex_lock);
+
+void rust_helper_xa_init_flags(struct xarray *xa, gfp_t flags)
+{
+	xa_init_flags(xa, flags);
+}
+EXPORT_SYMBOL_GPL(rust_helper_xa_init_flags);
+
+bool rust_helper_xa_empty(struct xarray *xa)
+{
+	return xa_empty(xa);
+}
+EXPORT_SYMBOL_GPL(rust_helper_xa_empty);
+
+int rust_helper_xa_alloc(struct xarray *xa, u32 *id, void *entry, struct xa_limit limit, gfp_t gfp)
+{
+	return xa_alloc(xa, id, entry, limit, gfp);
+}
+EXPORT_SYMBOL_GPL(rust_helper_xa_alloc);
+
+void rust_helper_xa_lock(struct xarray *xa)
+{
+	xa_lock(xa);
+}
+EXPORT_SYMBOL_GPL(rust_helper_xa_lock);
+
+void rust_helper_xa_unlock(struct xarray *xa)
+{
+	xa_unlock(xa);
+}
+EXPORT_SYMBOL_GPL(rust_helper_xa_unlock);
+
+int rust_helper_xa_err(void *entry)
+{
+	return xa_err(entry);
+}
+EXPORT_SYMBOL_GPL(rust_helper_xa_err);
 
 void rust_helper_amba_set_drvdata(struct amba_device *dev, void *data)
 {
