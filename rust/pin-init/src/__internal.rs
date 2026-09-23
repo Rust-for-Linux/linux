ad5545276a22ccd6061829b9e697edff6c910531
Gary Guo <gary@garyguo.net>
2026-09-23 19:20:14 +0100

    rust: pin-init: internal: make `__init_data` and `__pin_data` safe
    Remove the unsafe markers of `__init_data` and `__pin_data`. These methods
    are for inference help and does not need to be unsafe.
    
    The `HasInitData` cannot be implemented outside pin-init, so the `unsafe
    trait` marker is not necessary.
    
    Link: https://patch.msgid.link/20260923-dev-pin-data-diag-v1-1-e4d7193569e5@garyguo.net
    Signed-off-by: Gary Guo <gary@garyguo.net>

--- rust/pin-init/src/__internal.rs
+++ rust/pin-init/src/__internal.rs
@@ -65,12 +65,12 @@ impl InitOk {
 ///
 /// # Safety
 ///
-/// Only the `init` module is allowed to use this trait.
+/// `pin-init` relies on the correctness of the helper functions defined on `PinData`.
+/// Thus, only the `#[pin_data]` can implement this trait.
 pub unsafe trait HasPinData {
     type PinData;
 
-    #[expect(clippy::missing_safety_doc)]
-    unsafe fn __pin_data() -> Self::PinData;
+    fn __pin_data() -> Self::PinData;
 }
 
 /// This trait is automatically implemented for every type. It aims to provide the same type
@@ -82,8 +82,7 @@ pub unsafe trait HasPinData {
 pub unsafe trait HasInitData {
     type InitData;
 
-    #[expect(clippy::missing_safety_doc)]
-    unsafe fn __init_data() -> Self::InitData;
+    fn __init_data() -> Self::InitData;
 }
 
 pub struct AllData<T: ?Sized>(PhantomInvariant<T>);
@@ -113,7 +112,7 @@ unsafe impl<T: ?Sized> HasInitData for T {
     type InitData = AllData<T>;
 
     #[inline]
-    unsafe fn __init_data() -> Self::InitData {
+    fn __init_data() -> Self::InitData {
         AllData(PhantomInvariant::new())
     }
 }
